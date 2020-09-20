502f9c689aadab67e0f403807dfd308213c809e9
zaidsaeed <zaidsaeed>
2020-09-20 15:32:52 -0400

    db connection works well now, some refactoring is needed

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const { Sequelize } = require("sequelize");
 
 const app = express();
+
+const sequelize = new Sequelize("postgres", "postgres", "password", {
+  host: "twitter-clone-db.cezw9lbsfclc.us-west-2.rds.amazonaws.com",
+  dialect: "postgres",
+  port: 5432,
+});
+
+//DB Test
+sequelize
+  .authenticate()
+  .then(() => console.log("Connection has been established successfully."))
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
