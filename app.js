0e6d6958f62817a9a4951f2f3dad3c1bf594dbc5
zaidsaeed <zaidsaeed>
2020-09-25 02:43:08 -0400

    added logging information

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRoutes = require("./routes/User");
 const messageRoutes = require("./routes/Message");
+const logger = require("morgan");
 
 const bodyParser = require("body-parser");
 
@@ -8,6 +9,9 @@ const app = express();
 //Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//logger
+app.use(logger(":method :remote-addr :date[clf]"));
 
 app.use("/users", userRoutes);
 app.use("/messages", messageRoutes);
